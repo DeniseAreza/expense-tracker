8b8eae364e41c60c51e86c750a8d58d9a08d5853
DeniseAreza <51269054+DeniseAreza@users.noreply.github.com>
2020-08-08 20:57:18 +0800

    Day 4: Messed up with the SQLite3, but my idea on how I could get the amount, category, and message from the user and save it to the database is still a bit confusing for me.

--- main.py
+++ main.py
@@ -1,57 +1,57 @@
-import sqlite3 as db
+import sqlite3
 
-def init():
-    # Initialize a new database to store the expenditures
+def expenses():
+    # create connection
 
-    # Connect the database 
-    connectDB = db.connect("spend.db")
+    connection = sqlite3.connect('money.db')
 
-    # This will enable us to create queries in our database
-    cursor = connectDB.cursor()
+    # create cursor
+
+    cursor = connection.cursor()
+
+    # create table
+
+    expenses = """CREATE TABLE IF NOT EXISTS
+    expenses(amount FLOAT, category TEXT, message TEXT, date TEXT)"""
+
+    cursor.execute(expenses)
     
-    # create table command
-    sql = ''' CREATE TABLE IN NOT EXISTS expenses (
-        amount number,
-        category string,
-        message string,
-        date string 
-    )'''
 
-    # execute the command
-    cursor.execute(sql)
+def log(amount, category, message = ''):
+    # the inserting command must be here in this function
+    # amount : text
+    # category : text
+    # message : text
+    # date : text
 
-    # To store that information permanently
-    connectDB.commit()
-
-
-def log(amount, category, message = '' ):
-    # this function logs the expenditure in the database.
-    # amount : number
-    # category : string
-    # messsage : (optional) string
-
-    # Initializing the date
-    # import the datetiem module
+    # import datetime module
     from datetime import datetime
     date = str(datetime.now())
-    # Connect the database 
-    connectDB = db.connect("spend.db")
 
-    # This will enable us to create queries in our database
-    cursor = connectDB.cursor()
-    
-    # create table command
-    sql = ''' INSERT INTO expenses VALUES (
-         {},
-        '{}',
-        '{}',
-        '{}'
-    )'''.format(amount, category, message, date)
+    # connect to database
 
-    # execute the command
+    connection = sqlite3.connect("money.db")
+
+    # create a cursor
+
+    cursor = connection.cursor()
+
+    # insert values to the table
+
+    sql = """
+    INSERT INTO expenses (amount, category, message, date) VALUES (
+        ?,
+        ?,
+        ?,
+        ?
+    )"""
     cursor.execute(sql)
 
-    # To store that information permanently
-    connectDB.commit()
-    
-log(120, "transport", "naglakad lang talaga ako")
+    # get results
+
+    cursor.execute('SELECT * FROM expenses')
+
+    results = cursor.fetchall()
+
+    print(results)
+
